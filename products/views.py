2e3741822d34ecb8284ac4ebf8c691b99c545593
amfairley <adamfairley1990@gmail.com>
2024-08-10 09:30:05 +0100

    sorting: sort by category name, display number of results and sort option

--- products/views.py
+++ products/views.py
@@ -26,7 +26,9 @@ def all_products(request):
                 # lower_name is a field we are about to create with annotation
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            # Sort by category name, not id number
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
