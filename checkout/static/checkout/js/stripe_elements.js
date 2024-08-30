37fedfe5d805664def7ea4b3d79cc295f21ad618
amfairley <adamfairley1990@gmail.com>
2024-08-30 07:20:55 +0100

    stripe: error display functionality, add keys to settings

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -28,5 +28,23 @@ var style = {
         iconColor: '#dc3545'
     }
 };
+// Create card element
 var card = elements.create('card', {style: style});
+// Mount the card to the card-element 
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
